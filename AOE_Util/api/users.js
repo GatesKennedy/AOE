44f0d3e3a41dbaa600fc1bfca29d6975f4f61aeb
GatesKennedy <conor@esccollective.com>
2019-08-01 22:28:37 -0700

    MOD: default.json json Web Token ; MOD: api/users.js return JWT to user upon registration

--- AOE_Util/api/users.js
+++ AOE_Util/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {
     check,
     validationResult
@@ -69,12 +71,27 @@ router.post('/', [
             await user.save();
 
             //  Return jsonWebToken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User Registered');
-
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'), {
+                    expiresIn: 360000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({
+                        token
+                    });
+                }
+            );
         } catch (err) {
             console.error(err.message);
-            res.status(500).send('Server error');
+            res.status(500).send('MSG: Server error');
 
         }
 
