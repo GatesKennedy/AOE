6625e645c963cdea68cf76262e6bad7fa96f90da
GatesKennedy <conor@esccollective.com>
2019-08-11 22:53:48 -0700

    ADD: models/Account.js

---
+++ AOE_Util/models/Account.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const AccountSchema = new mongoose.Schema({
+    //      Back
+    //  =============
+    userPref: {
+        appList: {
+            type: [String],
+            required: true
+        },
+        dateSignup: {
+            type: Date,
+            required: true
+        },
+        usageStreak: {
+            type: [Number],
+            required: true
+        },
+        appsCreated: {
+            type: [String]
+        }
+    },
+    //      Front
+    //  =============
+    userPers: {
+        bio: {
+            type: String
+        },
+        pic: {
+            data: Buffer,
+            contentType: String
+        },
+        promo: {
+            type: String
+        }
+    }
+});
