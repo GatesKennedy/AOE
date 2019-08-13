ba0d83ca00df40b4310b2f0888f8b24f68a6b67a
GatesKennedy <conor@esccollective.com>
2019-08-13 01:28:18 -0700

    MOD: api/posts.js , define route to delete a comment from a post

--- AOE_Util/api/posts.js
+++ AOE_Util/api/posts.js
@@ -146,7 +146,7 @@ router.put('/like/:id', auth, async (req, res) => {
 });
 
 //  @route      PUT api/posts/:id
-//  @desc       Like a post
+//  @desc       unLike a post
 //  @access     Private
 router.put('/unlike/:id', auth, async (req, res) => {
     try {
@@ -207,5 +207,48 @@ router.post('/comment/:id', [auth, [
     }
 });
 
+//  @route      DELETE api/posts/comment/:id/:comment_id
+//  @desc       Delete a comment
+//  @access     Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        //  Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        //  Check comment
+        if (!comment) {
+            return res.status(404).json({
+                msg: 'Comment not found'
+            });
+        }
+
+        //  Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                msg: 'User not authorized'
+            });
+        }
+
+        //  Get remove index
+        const removeIndex = post.comments
+            .map(comment => comment.user.toString())
+            .indexOf(req.user.id);
+
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(500).json({
+                msg: 'Comment not found'
+            });
+        }
+        res.status(500).send('MSG: Server Error - cant post... SARS');
+    }
+});
 
 module.exports = router;
