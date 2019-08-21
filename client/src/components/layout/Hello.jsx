6e4098334c9cd9493efe115eda633d01b844417e
GatesKennedy <conor@esccollective.com>
2019-08-20 23:55:14 -0700

    MOD: src/app.css , use compiled scss for react styling // MOD: layout/Hello.jsx , layout/Navbar.jsx , implement components from html prelim designs

--- client/src/components/layout/Hello.jsx
+++ client/src/components/layout/Hello.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Hello = () => {
-  return <div />;
+  return (
+    <div class='hello center'>
+      <h2 class='greeting center'>hello, it's great to see You.</h2>
+    </div>
+  );
 };
 
 export default Hello;
--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -3,26 +3,32 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='navi light-s1'>
-      <ul>
-        <li>
-          <Link to='/dash'>Self</Link>
-        </li>
-        <li>
-          <Link to='/ctrl'>ctrl</Link>
-        </li>
-        <li>
-          <Link to='/open'>they</Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link to='/Login'>login</Link>
-        </li>
-        <li>
-          <Link to='/Register'>register</Link>
-        </li>
-      </ul>
+    <nav className='navi bg-dark'>
+      <div className='menu'>
+        <a href='aoe.html'>
+          {' '}
+          <i className='fas fa-infinity' />
+        </a>
+      </div>
+      <div className='center'>
+        <ul>
+          <li>
+            <a href='dash.hmtl'>dash</a>
+          </li>
+          <li>
+            <a href='ctrl.hmtl'>ctrl</a>
+          </li>
+          <li>
+            <a href='open.hmtl'>open</a>
+          </li>
+        </ul>
+      </div>
+      <div className='menu'>
+        <a href='curious.html'>
+          {' '}
+          <i className='fas fa-wave-square' />
+        </a>
+      </div>
     </nav>
   );
 };
