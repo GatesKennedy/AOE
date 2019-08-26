2c46f87e5196f3c59d80f797accf49f47ea98590
GatesKennedy <conor@esccollective.com>
2019-08-26 14:10:56 -0700

    MOD: /auth/Login.jsx , add form functionality , implement Hook 'useState'

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,31 +1,59 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const { username, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password) {
+      console.log('MSG: password confirmation failed');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Fragment>
       <section className='theme dark hello center'>
         <div className='greeting center'>
           <h2 className='msg center'>welcome back, Friend</h2>
         </div>
-        <div className='center'>
+        <form className='center' onSubmit={e => onSubmit(e)}>
           <ul className='column'>
             <li>
               <input
-                className='center input'
+                name='username'
                 type='text'
+                value={username}
+                onChange={e => onChange(e)}
                 placeholder='username'
+                className='center input'
               />
             </li>
             <li>
               <input
-                className='center input'
-                type='text'
+                name='username'
+                type='password'
+                value={password}
+                onChange={e => onChange(e)}
                 placeholder='password'
+                className='center input'
               />
             </li>
+            <li className='center options'>
+              <input type='submit' value='login' className='btn submit light' />
+            </li>
           </ul>
-        </div>
+        </form>
       </section>
     </Fragment>
   );
