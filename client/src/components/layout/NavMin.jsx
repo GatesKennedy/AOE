5018de27dbd877b0d61c6c8e56e9d4bf0fa24d4f
GatesKennedy <conor@esccollective.com>
2019-08-25 19:29:32 -0700

    NEW: dist/navmin.html, html template for unauthenticated version of navbar.jsx // MOD: components/../NavMin.jsx, implement .html template to .jsx

--- client/src/components/layout/NavMin.jsx
+++ client/src/components/layout/NavMin.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const NavMin = () => {
-  return <div />;
+  return (
+    <nav className='navi dark'>
+      <div className='menu'>
+        <a href='aoe.html'>
+          {' '}
+          <i className='light fas fa-infinity' />
+        </a>
+      </div>
+      <div className='menu center'>
+        <h2 className='center msg dark'>.: a o e :.</h2>
+      </div>
+      <div className='menu'>
+        <a href='curious.html'>
+          {' '}
+          <i className='light fas fa-wave-square' />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default NavMin;
