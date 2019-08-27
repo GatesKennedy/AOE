a2ab5ccd151b1325d94d7c901b45560bbb27bb54
GatesKennedy <conor@esccollective.com>
2019-08-26 21:10:57 -0700

    NEW: /src/store.js , /src/reducers/index.js , create store for redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+// === comps ===
 import Hello from './components/main/Hello';
 import Dash from './components/main/Dash';
 import Ctrl from './components/main/Ctrl';
 import Open from './components/main/Open';
 import Join from './components/auth/Join';
 import Login from './components/auth/Login';
+// === REDUX ===
+import { Provider } from 'react-redux';
+import store from './store';
+// === Style ===
+import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Route exact path='/' component={Hello} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/join' component={Join} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-      <Route exact path='/dash' component={Dash} />
-      <Route exact path='/ctrl' component={Ctrl} />
-      <Route exact path='/open' component={Open} />
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Route exact path='/' component={Hello} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/join' component={Join} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+        <Route exact path='/dash' component={Dash} />
+        <Route exact path='/ctrl' component={Ctrl} />
+        <Route exact path='/open' component={Open} />
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
