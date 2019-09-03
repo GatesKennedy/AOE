28ae4e7f19c04130b5e31a0f5949d83ec9b9698d
GatesKennedy <conor@esccollective.com>
2019-09-03 05:58:55 -0700

    NEW: /components/self/CreateProfile.js ,, rough mock up of profile fields // MOD: /models/Profile.js ,, redefine profile fields

--- client/src/components/self/CreateProfile.js
+++ client/src/components/self/CreateProfile.js
@@ -1,11 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const CreateProfile = props => {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    bio,
+    website: '',
+    location: '',
+    status: '',
+    interests: ''
+  });
 
-  return <div></div>;
+  const { bio, website, location, status, interests } = formData;
+
+  return (
+    <Fragment>
+      <div className='profile form input-text'>
+        <textarea
+          placeholder='bio... err, what do you care about?'
+          name='bio'
+        ></textarea>
+      </div>
+      <div className='profile form input-text'>
+        <input type='text' placeholder='Website..' name='website' />
+        <small className='form-text'>personal or associated website..</small>
+      </div>
+      <div className='profile form input-text'>
+        <input type='text' placeholder='Location' name='location' />
+        <small className='form-text'>
+          where are you in Portland? (eg. inner se)
+        </small>
+      </div>
+      <div className='profile form'>
+        <input type='submit' value='save' className='btn dark' />
+        <Link to='/dash' className='btn dark'>
+          bail
+        </Link>
+      </div>
+    </Fragment>
+  );
 };
 
 CreateProfile.propTypes = {};
